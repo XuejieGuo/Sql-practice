377846fb963310565413e7d6d9a47f1f1f379888
XUEJIE GUO <54657316+XuejieGuo@users.noreply.github.com>
2021-02-10 12:21:22 -0800

    Update 185_DepartmentTopThreeSalaries.sql

--- Leetcode/185_DepartmentTopThreeSalaries.sql
+++ Leetcode/185_DepartmentTopThreeSalaries.sql
@@ -1,11 +1,11 @@
 WITH l AS(
 SELECT Name, Salary, DepartmentId 
 FROM(
-    SELECT *, rank() OVER (PARTITION BY DepartmentId 
+    SELECT *, dense_rank() OVER (PARTITION BY DepartmentId 
                            ORDER BY Salary DESC) AS rk
     FROM Employee
 ) AS a
-WHERE rk = 1
+WHERE rk < 4
 )
 
 SELECT Department.Name AS Department, 
